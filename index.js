cd3928329a01ed0f398c5435bb8e3380dc642145
Saifullah Monsur Jewel <jewellahmed97@gmail.com>
2021-04-02 15:41:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5055;
+
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+// console.log(process.env.DB_USER)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbtgk.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+
+  res.send('Hello World')
+})
+
+
+client.connect(err => {
+  // console.log(err)
+  const productsCollection = client.db("market").collection("goods");
+
+
+  app.get('/products', (req, res) => {
+    productsCollection.find()
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+    })
+
+
+    app.post('/addProduct', (req, res) => {
+
+      const newProduct = req.body;
+      console.log('adding new product:', newProduct);
+      productsCollection.insertOne(newProduct)
+        .then(result => {
+
+          console.log('Insert count', result.insertedCount)
+          res.send(result.insertedCount > 0)
+        })
+
+    })
+
+
+  });
+
+
+
+
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
